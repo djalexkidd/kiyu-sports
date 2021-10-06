c80d8e9216bd73187a08c89b81b07c6f486e881a
djalexkidd <alexpyr@outlook.fr>
2021-10-06 23:39:43 +0200

    Ajout d'une animation pour Kiyu

--- scripts/Kiyu.gd
+++ scripts/Kiyu.gd
@@ -2,9 +2,16 @@ extends KinematicBody2D
 
 var velocity = Vector2(0,0)
 var SPEED = 180 #Vitesse du joueur
+var num_frames
+var current_frame = 0
+
+func _ready():
+	num_frames = $AnimatedSprite.get_sprite_frames().get_frame_count("walk")
 
 func _physics_process(delta):
 	if Input.is_action_just_pressed("fire"):
 		velocity.x = SPEED
+		current_frame = (current_frame + 1) % num_frames
+		$AnimatedSprite.set_frame(current_frame)
 	velocity = move_and_slide(velocity)
 	velocity.x = lerp(velocity.x,0,0.2)
