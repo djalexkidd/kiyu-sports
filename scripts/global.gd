9d42f387a8120a2f1a56a717dca8f0cb5eaa68e9
djalexkidd <alexpyr@outlook.fr>
2021-10-11 22:16:58 +0200

    PTN YES ÇA MARCHE

--- scripts/global.gd
+++ scripts/global.gd
@@ -2,12 +2,11 @@ extends Node
 
 onready var username
 onready var score
-onready var character = 2
+onready var character = 1
 
 func char_select():
 	match character:
 		1:
 			return "res://scenes/Kiyu.tscn"
 		2:
-			
 			return "res://scenes/Roblox.tscn"
