55a57605e44415d71e2420a08e14219c1b609e1f
djalexkidd <alexpyr@outlook.fr>
2021-10-11 16:34:37 +0200

    Oh ptn on peut changer de personnage

--- scripts/Field.gd
+++ scripts/Field.gd
@@ -1,5 +1,12 @@
 extends Node2D
 
+var player_character_path = Global.char_select()
+
+func _ready():
+	var player_character = load(player_character_path).instance()
+	player_character.position = Vector2(225, 608)
+	add_child(player_character)
+
 func _on_Timer_timeout():
 	$HUD/Speed.text = "%0.f" % $Kiyu.velocity.x
 
--- scripts/global.gd
+++ scripts/global.gd
@@ -2,3 +2,12 @@ extends Node
 
 onready var username
 onready var score
+onready var character = 2
+
+func char_select():
+	match character:
+		1:
+			return "res://scenes/Kiyu.tscn"
+		2:
+			
+			return "res://scenes/Roblox.tscn"
