faa9144a40619f59d2af28a31e429d7f5c36f1f6
djalexkidd <alexpyr@outlook.fr>
2021-10-13 09:24:22 +0200

    Ajout d'un menu séparé pour les épreuves

--- scripts/menu/NameEntry.gd
+++ scripts/menu/NameEntry.gd
@@ -1,26 +1,7 @@
 extends Control
 
-func _ready():
-	refresh_char()
+func _on_PlayButton_pressed():
+	get_tree().change_scene("res://scenes/menu/GameSelect.tscn")
 
-func _on_NextCharButton_pressed():
-	Global.character += 1
-	refresh_char()
-
-func _on_PreviousCharButton_pressed():
-	Global.character -= 1
-	refresh_char()
-
-func refresh_char():
-	$AnimatedSprite.play(str(Global.character))
-	if Global.character > 2:
-		Global.character -= 1
-	elif Global.character < 1:
-		Global.character += 1
-	
-	if Global.character == 2:
-		$AnimatedSprite.scale.x = 1.5
-		$AnimatedSprite.scale.y = 1.5
-	else:
-		$AnimatedSprite.scale.x = 4
-		$AnimatedSprite.scale.y = 4
+func _on_QuitButton_pressed():
+	get_tree().quit()
